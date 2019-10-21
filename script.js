e428eb543566d597ee4039b1268415e32808a3e9
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-21 11:10:16 -0400

    created HTML & CSS, started JS

---
+++ script.js
@@ -0,0 +1,18 @@
+//Pseudocode
+// Define the elements
+// 	create a variable for each box
+const A1 = document.querySelector("#A1");
+const A2 = document.querySelector("#A2");
+const A3 = document.querySelector("#A3");
+const B1 = document.querySelector("#B1");
+const B2 = document.querySelector("#B2");
+const B3 = document.querySelector("#B3");
+const C1 = document.querySelector("#C1");
+const C2 = document.querySelector("#C2");
+const C3 = document.querySelector("#C3");
+const backgroundColor = document
+
+function determineWinner () {
+//determine all scenarios; conditional statement
+
+};
