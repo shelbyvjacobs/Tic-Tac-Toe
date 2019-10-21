48873fd5395b387e27af0f1fe9e38da45c41d150
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-21 12:10:54 -0400

    started determineWinner function

--- script.js
+++ script.js
@@ -14,14 +14,36 @@ const C2 = document.querySelector("#C2");
 const C3 = document.querySelector("#C3");
 let redOrBlue = 0;
 
-// function determineWinner () {
-// //determine all scenarios; conditional statement
-// 	if (A1.style.backgroundColor == "red" && A2.style.backgroundColor == "red" && A3.style.backgroundColor == "red"){
-// 		console.log("red team wins!")
-// 	} else if (B1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && B3.style.backgroundColor == "red"){
-
-// 	}
-// };
+function determineWinner () {
+//determine all scenarios; conditional statement
+	//row1
+	if (A1.style.backgroundColor == "red" && A2.style.backgroundColor == "red" && A3.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//row2
+	} else if (B1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && B3.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//row3
+	} else if (C1.style.backgroundColor == "red" && C2.style.backgroundColor == "red" && C3.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//column1
+	} else if (A1.style.backgroundColor == "red" && B1.style.backgroundColor == "red" && C1.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//column2
+	} else if (A2.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && C2.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//column3
+	} else if (A3.style.backgroundColor == "red" && B3.style.backgroundColor == "red" && C3.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//diagonal from top left
+	} else if (A1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && C3.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	//diagonal from top right
+	} else if (A3.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && C1.style.backgroundColor == "red"){
+		console.log("Red team wins!")
+	} else {
+		console.log("blah blah blah")
+	}
+};
 
 //eventlisteners & determine if it's red or blue
 
@@ -31,9 +53,13 @@ let redOrBlue = 0;
 			if (redOrBlue%2 === 0){
 				this.style.backgroundColor = "red"
 				redOrBlue = redOrBlue +=1;
+				console.log("Blue team's turn.")
+					//change to .innerText
 			} else {
 				this.style.backgroundColor = "blue"
 				redOrBlue = redOrBlue +=1;
+				console.log("Red team's turn.")
+					//change to .innerText
 			} 
 		})
 	}
