093ebacd3e8238ee18fbb35dba383f3ec7b3f4b6
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-21 14:19:03 -0400

    display winner of game

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Pseudocode
-// Define the elements
+// Pseudocode
+
 // 	create a variable for each box
 const gameBoard = document.querySelector("#gameBoard");
 const boxes = document.querySelectorAll(".box");
@@ -14,10 +14,11 @@ const C2 = document.querySelector("#C2");
 const C3 = document.querySelector("#C3");
 const turnSpan = document.querySelector("#turnSpan")
 let turn = document.querySelector("#turn")
+let winner = document.querySelector("h2")
 let redOrBlue = 0;
 
-function determineWinner () {
 //determine all scenarios; conditional statement
+function determineWinner () {
 	if (
 		(A1.style.backgroundColor == "red" && A2.style.backgroundColor == "red" && A3.style.backgroundColor == "red")||
 		(B1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && B3.style.backgroundColor == "red")||
@@ -28,7 +29,8 @@ function determineWinner () {
 		(A1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && C3.style.backgroundColor == "red")||
 		(A3.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && C1.style.backgroundColor == "red")
 	){
-		turn.innerText = "Red team wins!" //doesn't work
+		winner.innerText = "Red team wins!"
+		turn.style.display = "none"
 		console.log("Red team wins!")
 	} else if (
 		(A1.style.backgroundColor == "blue" && A2.style.backgroundColor == "blue" && A3.style.backgroundColor == "blue")||
@@ -40,30 +42,34 @@ function determineWinner () {
 		(A1.style.backgroundColor == "blue" && B2.style.backgroundColor == "blue" && C3.style.backgroundColor == "blue")||
 		(A3.style.backgroundColor == "blue" && B2.style.backgroundColor == "blue" && C1.style.backgroundColor == "blue")
 	){
-		turn.innerText = "Blue team wins!" //doesn't work
+		winner.innerText = "Blue team wins!"
+		turn.style.color = "none"
 		console.log("Blue team wins!")
 	} else {
 		console.log("Keep Playing!")
 	}
 };
 
-determineWinner();
-
 //eventlisteners & determine if it's red or blue
+for (let i=0; i<boxes.length; i++){
+	boxes[i].addEventListener("click", function(evt){
+		evt.preventDefault();
+		if (redOrBlue%2 === 0){
+			this.style.backgroundColor = "red"
+			determineWinner();
+			redOrBlue = redOrBlue +=1;
+			turn.innerText = "Blue team's turn.";
+			//removeEventListener
+		} else {
+			this.style.backgroundColor = "blue"
+			determineWinner();
+			redOrBlue = redOrBlue +=1;
+			turn.innerText = "Red team's turn.";
+			//removeEventListener
+		} 
+	})
+}
 
-	for (let i=0; i<boxes.length; i++){
-		boxes[i].addEventListener("click", function(evt){
-			evt.preventDefault();
-			if (redOrBlue%2 === 0){
-				this.style.backgroundColor = "red"
-				determineWinner();
-				redOrBlue = redOrBlue +=1;
-				turn.innerText = "Blue team's turn.";
-			} else {
-				this.style.backgroundColor = "blue"
-				determineWinner();
-				redOrBlue = redOrBlue +=1;
-				turn.innerText = "Red team's turn.";
-			} 
-		})
-	}
+
+//make sure you can't change the square's color!!!
+
