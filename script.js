78e3816423564344768b4bab1f208ee332ff9cab
Shelby Jacobs <shelbyvjacobs@gmail.com>
2019-10-21 11:24:13 -0400

    added classes to winning combinations

--- script.js
+++ script.js
@@ -10,9 +10,25 @@ const B3 = document.querySelector("#B3");
 const C1 = document.querySelector("#C1");
 const C2 = document.querySelector("#C2");
 const C3 = document.querySelector("#C3");
-const backgroundColor = document
+//create variable for each row, column, and diagonal
+const column1 = document.querySelectorAll(".column1");
+const column2 = document.querySelectorAll(".column2");
+const column3 = document.querySelectorAll(".column3");
+const row1 = document.querySelectorAll(".row1");
+const row2 = document.querySelectorAll(".row2");
+const row3 = document.querySelectorAll(".row3");
+const diagonalTopLeft = document.querySelectorAll(".diagonalTopLeft");
+const diagonalTopRight = document.querySelectorAll(".diagonalTopRight");
+
+// console.log(diagonalTopRight); //returns nodeList
+// console.log(column2); //returns nodeList
+// console.log(A3);
 
 function determineWinner () {
 //determine all scenarios; conditional statement
+	if (A1.style.backgroundColor == "red" && A2.style.backgroundColor == "red" && A3.style.backgroundColor == "red"){
+		console.log("red team wins!")
+	} else if (B1.style.backgroundColor == "red" && B2.style.backgroundColor == "red" && B3.style.backgroundColor == "red"){
 
+	}
 };
